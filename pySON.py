68ecaa48a8a8d2e32cb5a4da984c06958bded03f
willh99 <willh99@gwmail.gwu.edu>
2018-02-28 13:39:20 -0500

    Full commit of all working codes on the RaspPi

--- pySON.py
+++ pySON.py
@@ -9,43 +9,48 @@ def read_json(filename):
         return -1
     try:
         with open(filename, 'r') as f:
-            print("File \"", filename, "\" found", sep='')
             data = json.load(f)
             return data
     except FileNotFoundError:
         print("File Not Found")
         return -1
 
-
-def append_json(data):
-    with open('v_log.json', 'w', encoding='utf-8') as f:
+def append_json(data, filename):
+    with open(filename, mode='w') as f:
         json.dump(data, f, indent=2)
-        print("wrote to file")
 
 
-def create_status(buy, sell):
+def create_status(buy, sell, isprice):
     now = datetime.datetime.now()
     now = now.strftime('%d-%m-%Y %X')
     data = {"Sell": sell, "Buy": buy, "Timestamp": now}
-    with open('status.json', 'w', encoding='utf-8') as f:
+    if isprice:
+        filename = 'price_status.json'
+    else:
+        filename = 'status.json'
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2)
 
 
+
 if __name__ == "__main__":
+    '''
     json_list = []
     for x in range(0, 100):
         i = random.random()*12.8
         dictionary = {"Timestamp": time.asctime(time.localtime()),
                       "Voltage": round(i, 6)}
 
-        if len(json_list) >= 50:
+        if len(json_list) >= 10:
             json_list.pop(0)
         json_list.append(dictionary)
-        # time.sleep(.2)
+        #time.sleep(.2)
 
     append_json(json_list)
 
-    something = read_json('status.json')
+    something = read_json('g_log.json')
     if something is not -1:
-        print(json.dumps(something, indent=2))
+        print(something)
+    '''
+    create_status(False, False, None)
 
